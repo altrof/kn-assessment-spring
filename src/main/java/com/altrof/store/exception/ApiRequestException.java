90516cba1280551ece8fa92f3f6deda512988e4d
Aleksandr Trofimov <altrof.tech@gmail.com>
2022-04-13 23:01:59 +0300

    Adding a handler for api exceptions.

---
+++ src/main/java/com/altrof/store/exception/ApiRequestException.java
@@ -0,0 +1,8 @@
+package com.altrof.store.exception;
+
+public class ApiRequestException extends RuntimeException{
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+}
